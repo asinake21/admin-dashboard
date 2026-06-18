02c07faed298007b39f9151b4915ab9f22fee47f
Asinake <asinakehailie00@gmail.com>
2026-06-18 16:42:47 +0300

    Configure cloud database and production setup

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 27786, // <-- Make sure port is included!
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false // <-- This is necessary for Aiven connections
+  }
 });
 
 db.connect((error) => {
